a7f27b93d9a1ec6fa8591b91b51010c6171287a9
JKs162426 <jesus20jk@gmail.com>
2026-05-19 22:27:39 -0600

    Fix Swagger schemes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const { 
-    getAllContacts, 
-    getContactById,
-    createContact,
-    updateContact,
-    deleteContact 
+const {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContact
 } = require('../controllers/contacts');
 
 router.get('/', getAllContacts);
 router.get('/:id', getContactById);
-router.post('/', createContact);
+
+router.post('/', (req, res) => {
+    /* #swagger.parameters['body'] = 
+    { in: 'body',
+      required: true,
+      schema: { 
+      firstName: 'Jesus', 
+      lastName: 'Figueroa', 
+      email: 'jesus@email.com', 
+      favoriteColor: 'blue', 
+      birthday: '01/01/2000' 
+      } 
+    }*/
+
+    createContact(req, res)
+});
+
 router.put('/:id', updateContact);
+
 router.delete('/:id', deleteContact);
 
 module.exports = router;
